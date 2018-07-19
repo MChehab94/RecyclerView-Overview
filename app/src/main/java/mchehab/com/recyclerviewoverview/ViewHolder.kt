473eaa74cda1404b8c575d462183596de1c474a7
Mohamad Chehab <lemirmohammad.shehab@lau.edu>
2018-07-19 23:05:23 +0300

    Optimized imports

--- app/src/main/java/mchehab/com/recyclerviewoverview/ViewHolder.kt
+++ app/src/main/java/mchehab/com/recyclerviewoverview/ViewHolder.kt
@@ -2,9 +2,7 @@ package mchehab.com.recyclerviewoverview
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
-import com.squareup.picasso.Picasso
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private var textView: TextView = itemView.findViewById(R.id.textView)
